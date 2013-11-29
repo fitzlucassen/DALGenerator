34a2fe8f2c0f304b3ed977c4414d5eacaadd7c58
Titou <thibault.dulon@gmail.com>
2013-11-29 15:25:22 +0100

    maj delete

--- config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    define('DB','passangerv2');						    // database
-    define('HOST', 'localhost');					    // database host
-    define('USER', 'root');						    // user name 
-    define('PWD', '');							    // password
-    define('PATH_ENTITIES', 'C:/wamp/www/DALGenerator/Entity/');	    // The path where entities will be created
-    define('PATH_REPOSITORIES', 'C:/wamp/www/DALGenerator/Repository/');    // The path where repositories will be created
