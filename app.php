dd203c7b0f53ff6f759baa9f9d9d32e9264b7c17
Titou <thibault.dulon@gmail.com>
2013-11-29 12:03:11 +0100

    migration en class

--- app.php
+++ app.php
@@ -1,26 +1,26 @@
 <?php
 
-    require_once 'config.class.php';
-    require_once 'sql.class.php';
-    require_once 'utilities.class.php';
+    require_once 'library/config.class.php';
+    require_once 'library/sql.class.php';
+    require_once 'library/utilities.class.php';
 
-    $Config = Config::GetInstance();
+    $Config = DALGenerator\Config::GetInstance();
     
     /*************************
      * PUT YOUR CONFIGS HERE *
      *************************/
-    $Config->SetDB("passangerv2");					// database
-    $Config->SetHOST("localhost");					// database host
-    $Config->SetUSER("root");						// user name
-    $Config->SetPWD("");						// password
-    $Config->SetPATHENTITIES("C:/wamp/www/DALGenerator/Entity/");	// The path where entities will be created
-    $Config->SetPATHREPOSITORIES("C:/wamp/www/DALGenerator/Repository/");	// The path where repositories will be created
+    $Config->SetDB("passangerv2");					    // database
+    $Config->SetHOST("localhost");					    // database host
+    $Config->SetUSER("root");						    // user name
+    $Config->SetPWD("");						    // password
+    $Config->SetPATHENTITIES("C:/wamp/www/DALGenerator/Entity/");	    // The path where entities will be created
+    $Config->SetPATHREPOSITORIES("C:/wamp/www/DALGenerator/Repository/");   // The path where repositories will be created
     /*******
      * END *
      *******/
     
-    $Connexion = new Sql($Config->GetDB(), $Config->GetHOST(), $Config->GetUSER(), $Config->GetPWD());
-    $Utilities = new Utilities($Connexion);
+    $Connexion = new DALGenerator\Sql($Config->GetDB(), $Config->GetHOST(), $Config->GetUSER(), $Config->GetPWD());
+    $Utilities = new DALGenerator\Utilities($Connexion);
     $master_array = $Utilities->GetTablesArray();
 
     $Utilities->CreateClasses($Config->GetPATHENTITIES(), $Config->GetPATHREPOSITORIES());
