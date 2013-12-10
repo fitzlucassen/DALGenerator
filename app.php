b7c0f83872a1552f4895e568022f20ed01eff58e
Thibault <thibault.dulon@gmail.com>
2013-12-10 15:41:35 +0100

    execution en ligne de commande

--- app.php
+++ app.php
@@ -7,24 +7,39 @@
     $Config = fitzlucassen\DALGenerator\Config::getInstance();
     $fm = fitzlucassen\DALGenerator\FileManager::getInstance();
     
-    /*************************
-     * PUT YOUR CONFIGS HERE *
-     *************************/
-    $Config->setDB("passangerv2");					    // database
-    $Config->setHOST("localhost");					    // database host
-    $Config->setUSER("root");						    // user name
-    $Config->setPWD("");						    // password
-    $Config->setPATHENTITIES("C:/wamp/www/DALGenerator/Entity/");	    // The path where entities will be created
-    $Config->setPATHREPOSITORIES("C:/wamp/www/DALGenerator/Repository/");   // The path where repositories will be created
-    // If there is some links into your tables, you have to precise these right here.
-    // Example: you will have "getSongs" method into the "album" class, and you'll have "getALbum" instead of "getAlbumId" into "song" class
-    $Config->setLink(array('album' => array('song' => 'OneToMany'), 'song' => array('album' => 'OneToOne')));
-    /*******
-     * END *
-     *******/
-    
+    if(defined('STDIN')){
+	$Config->setDB($argv[1]);		// database
+	$Config->setHOST($argv[2]);		// database host
+	$Config->setUSER($argv[3]);		// user name
+	$Config->setPWD($argv[4]);		// password
+	$Config->setPATHENTITIES($argv[5]);	// The path where entities will be created
+	$Config->setPATHREPOSITORIES($argv[6]);	// The path where repositories will be created
+    }
+    else {
+	/*************************
+	 * PUT YOUR CONFIGS HERE *
+	 *************************/
+       $Config->setDB("passangerv2");					    // database
+       $Config->setHOST("localhost");					    // database host
+       $Config->setUSER("root");					    // user name
+       $Config->setPWD("");						    // password
+       $Config->setPATHENTITIES("C:/wamp/www/DALGenerator/Entity/");	    // The path where entities will be created
+       $Config->setPATHREPOSITORIES("C:/wamp/www/DALGenerator/Repository/");// The path where repositories will be created
+       
+       // If there is some links into your tables, you have to precise these right here.
+       // 
+       // Example: you will have "getSongs" method into the "album" class, and you'll have "getALbum" instead of "getAlbumId" into "song" class
+       $Config->setLink(array('album' => array('song' => 'OneToMany'), 'song' => array('album' => 'OneToOne')));
+       /*******
+	* END *
+	*******/
+    }
     $Connexion = new fitzlucassen\DALGenerator\Sql($Config->getDB(), $Config->getHOST(), $Config->getUSER(), $Config->getPWD());
     $Utilities = new fitzlucassen\DALGenerator\Utilities($Connexion, 2, array("_pdoHelper"));
     $master_array = $Utilities->getTablesArray();
 
     $Utilities->createClasses($Config->getPATHENTITIES(), $Config->getPATHREPOSITORIES(), $Config->getLink());
+    
+    if(defined('STDIN')){
+	exit(0);
+    }
